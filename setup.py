2be19676153de538ff64d86dd35b705dc92e3747
bheemdevhub <devhub@bheemverse.com>
2025-07-29 16:09:33 +0530

    chore: add and update setup.py for editable install

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='bheem-sales',
     version='1.0.0',
-    packages=find_packages(),
-    install_requires=[], # Add module-specific dependencies
+    packages=find_packages(include=["sales", "sales.*"]),
+    install_requires=[],
     include_package_data=True,
     description='Bheem Sales ERP module',
     author='Bheem Core Team',
